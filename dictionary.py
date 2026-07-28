f0ba5cd758fc61eddef8f757b990f9a3c73f1caf
karimeh <ka.sharifi.it@gmail.com>
2026-07-29 01:56:19 +0430

    Fruit dictionary exercise
    Create a dictionary of fruits and prices, change one price, remove one fruit, and display the remaining list.

---
+++ dictionary.py
@@ -0,0 +1,6 @@
+fruit_prices = {"apple": 1500 , "banana": 1000, "orange": 1200}
+fruit_prices ["banana"] = 1100
+print (fruit_prices ["banana"])
+fruit_prices.pop("apple")
+print(fruit_prices.get("apple" ,None ))
+print (fruit_prices)
