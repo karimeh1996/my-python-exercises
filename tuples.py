85e6ec17c44c3f988f126aca9f3b343f2ed32b6c
karimeh <ka.sharifi.it@gmail.com>
2026-07-31 02:39:43 +0430

    calculate Euclidean distance from point to origin using tuple

---
+++ tuples.py
@@ -0,0 +1,17 @@
+import math
+coordinates =  (3,4)
+x1 = 0
+y1 = 0
+x2 = coordinates [0]
+y2 = coordinates [1]
+x = x2-x1
+y = y2-y1
+x = x**2
+y = y**2
+z = x + y
+distance = math.sqrt(z)
+print (distance)
+
+
+
+
