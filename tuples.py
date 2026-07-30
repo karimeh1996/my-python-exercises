e257e3e6c1a0bd65b873ed5c6ea398da1ba8fb69
karimeh <ka.sharifi.it@gmail.com>
2026-07-31 03:00:50 +0430

    refactor: simplify distance calculation by removing redundant variables

--- tuples.py
+++ tuples.py
@@ -1,15 +1,8 @@
 import math
 coordinates =  (3,4)
-x1 = 0
-y1 = 0
 x2 = coordinates [0]
 y2 = coordinates [1]
-x = x2-x1
-y = y2-y1
-x = x**2
-y = y**2
-z = x + y
-distance = math.sqrt(z)
+distance = math.sqrt((x2**2)+(y2**2))
 print (distance)
 
 
