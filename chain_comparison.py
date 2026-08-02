1c6a8c66f7ff7cc6d7f943f232d2095d675473c1
karimeh <ka.sharifi.it@gmail.com>
2026-08-03 02:39:59 +0430

    add exercise for a < b <= c

---
+++ chain_comparison.py
@@ -0,0 +1,16 @@
+while True:
+ 
+ a = int (input())
+ b = int (input())
+ c = int (input())
+ if a < b <= c :
+     print (True)
+ else:
+     print (False)
+ m = input ("Do you want continue؟\n" "Yes\n" "NO\n")    
+ if m.lower() == "no":
+    break
+
+
+
+
