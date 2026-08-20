7f89addf4a11c6f4bb507af50dea1758c3d05be3
karimeh <ka.sharifi.it@gmail.com>
2026-08-20 20:44:29 +0430

    functions directory: core utility functions

--- conditionals/magic_number_3_5.py
+++ conditionals/magic_number_3_5.py
@@ -1,19 +1,4 @@
-n = 1
-while n != 0:
-    n = int (input("please Enter a number (Enter 0 for Exit): ")) 
-    if n==0:
-        print ("goodbye  👋")
-        
-    if n % 3 == 0 and n % 5 == 0:
-        print ("The number is legendary.")
-    elif n % 5 == 0:
-        print ("The number is cursed.")
-    elif n % 3 == 0:
-        print ("The number is magic.")
-    else:
-        print ("The number is normal")
-    
+def خوش‌آمدگویی(اسم):
+    print("سلام " + اسم)
 
-    
-
-
+خوش‌آمدگویی("علی")  # خروجی: سلام علی
---
+++ functions/greet_function.py
@@ -0,0 +1,7 @@
+def greet(Name):
+# This function gets a name and prints the welcome message 
+    return "Hello, " + Name+ "!"
+print(greet("Ali"))
+
+
+
