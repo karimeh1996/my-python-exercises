13dd713082b6e4470138fc007975a24da9313f66
karimeh <ka.sharifi.it@gmail.com>
2026-08-22 01:23:34 +0430

    add sum_of_squares function to calculate sum of two squares

---
+++ functions/sum_of_squares.py
@@ -0,0 +1,9 @@
+def sum_of_squares(a, b):
+    """
+    Calculates the sum of squares of two numbers
+    """
+    return (a**2)+(b**2)
+
+a= int (input())
+b= int (input())
+print (sum_of_squares(a, b))
