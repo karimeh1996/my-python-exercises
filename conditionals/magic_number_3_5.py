c7913078d5e42539aaae7500a1bc7e671bc3e645
karimeh <ka.sharifi.it@gmail.com>
2026-08-21 21:44:01 +0430

    restore magic_number_3_5.py to correct content

--- conditionals/magic_number_3_5.py
+++ conditionals/magic_number_3_5.py
@@ -1,4 +1,19 @@
-def خوش‌آمدگویی(اسم):
-    print("سلام " + اسم)
+n = 1
+while n != 0:
+    n = int (input("please Enter a number (Enter 0 for Exit): ")) 
+    if n==0:
+        print ("goodbye  👋")
+        
+    if n % 3 == 0 and n % 5 == 0:
+        print ("The number is legendary.")
+    elif n % 5 == 0:
+        print ("The number is cursed.")
+    elif n % 3 == 0:
+        print ("The number is magic.")
+    else:
+        print ("The number is normal")
+    
 
-خوش‌آمدگویی("علی")  # خروجی: سلام علی
+    
+
+
